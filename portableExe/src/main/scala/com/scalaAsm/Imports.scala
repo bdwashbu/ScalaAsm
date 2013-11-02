11f6104db2107f938c4324fe5cea3949915844b4
bdwashbu <bdwashbu@gmail.com>
2013-11-02 17:03:33 -0500

    Trying to make the imports section more understandable

--- portableExe/src/main/scala/com/scalaAsm/Imports.scala
+++ portableExe/src/main/scala/com/scalaAsm/Imports.scala
@@ -8,42 +8,58 @@ case class Extern(dllName: String, functionNames: Seq[String])
 
 case class Imports(val externs: Seq[Extern], val nonExterns: Seq[Extern], val offset: Int) extends ExeWriter {
 
+  type Address = Int
+  
+  case class ImageThunkDataPointer(address: Int)
+  case class ImportByNamePointer(address: Int)
+  
   case class ImageImportDescriptor (
-    val originalFirstThunk: Int,
+    val originalFirstThunk: ImageThunkDataPointer, // RVA to unbound IAT
     val timeStamp: Int,
-    val forwarderChain: Int,
+    val forwarderChain: Int, // -1 if no forwarders
     val importedDLLname: Int,
-    val firstThunk: Int) extends ExeWriter {
+    val firstThunk: ImageThunkDataPointer) // points to an array of RVAs
+      extends ExeWriter {
 
     def write(stream: DataOutputStream) {
-      write(stream, originalFirstThunk)
+      write(stream, originalFirstThunk.address)
       write(stream, timeStamp)
       write(stream, forwarderChain)
       write(stream, importedDLLname)
-      write(stream, firstThunk)
+      write(stream, firstThunk.address)
     }
+  }
+  
+  object ImageImportDescriptor 
+  {
+    val size = 20;
   }
 
   def link: CompiledImports = {
 
     val imports = externs ++ nonExterns
+    val numImportsPlusNull = imports.size + 1
 
     def padNames(name: String) = if (name.size % 2 == 0) Array[Byte](0x00, 0x00) else Array[Byte](0x00)
 
     def getDllNames(x: Seq[Extern]): List[String] = x.flatMap(x => List(x.dllName)).toList
     def getFunctionNames(x: Seq[Extern]): List[String] = x.flatMap(_.functionNames).toList
 
-    case class BoundImport(val boundImportDescriptors: Seq[ImageImportDescriptor], val importAddressTable: Seq[Int])
+    case class BoundImport(val boundImportDescriptors: Seq[ImageImportDescriptor], val importAddressList: Seq[Int])
 
     def getBoundImports(): BoundImport = {
 
       val initalLookupTableRVA: Map[String, Int] = Map.empty
 
-      val nameRVAs: Map[String, Int] = {
+      type ImageImportAddress = Int
+      
+      val nameRVAs: Map[String, ImageImportAddress] = {
         var position = 0
         val flattenedFcns: Seq[String] = imports.flatMap(x => x.functionNames ++ Seq(x.dllName))
+        
         flattenedFcns.foldLeft(initalLookupTableRVA)((prev, name) => {
           val skipHint = if (getDllNames(imports).contains(name)) 0 else 2
+          
           val result = prev ++ Map(name -> (position + skipHint))
           position += name.length + padNames(name).length + skipHint
           result
@@ -63,22 +79,22 @@ case class Imports(val externs: Seq[Extern], val nonExterns: Seq[Extern], val of
       val importFunctionNames = getFunctionNames(imports)
       val sizeOfAddrTable = importFunctionNames.size * 4 + getDllNames(imports).size * 4
       
-      def getAddress(name: String) = offset + nameRVAs(name) + sizeOfAddrTable * 2 + (imports.size + 1) * 20 - 2
-      
-      def toAddressTable(extern: Extern) = extern.functionNames.map(getAddress) :+ 0
+      def getRVAAddress(name: String): Address = offset + nameRVAs(name) + sizeOfAddrTable * 2 + numImportsPlusNull * ImageImportDescriptor.size - 2
 
       val boundImports = getDllNames(imports).map { dllName =>
 
-        val lookupAddr = offset + lookupTableRVAs(dllName) + (imports.size + 1) * 20 // assumes imports.size is the number of tables
+        val lookupAddr = offset + lookupTableRVAs(dllName) + numImportsPlusNull * ImageImportDescriptor.size // assumes imports.size is the number of tables
 
         ImageImportDescriptor(
-          originalFirstThunk = lookupAddr,
+          originalFirstThunk = ImageThunkDataPointer(lookupAddr),
           timeStamp = 0x00000000,
           forwarderChain = 0x00000000,
-          importedDLLname = offset + nameRVAs(dllName) + sizeOfAddrTable * 2 + (imports.size + 1) * 20,
-          firstThunk = lookupAddr + sizeOfAddrTable)
-      } :+ ImageImportDescriptor(0, 0, 0, 0, 0) // terminating descriptor
+          importedDLLname = offset + nameRVAs(dllName) + sizeOfAddrTable * 2 + numImportsPlusNull * ImageImportDescriptor.size,
+          firstThunk = ImageThunkDataPointer(lookupAddr + sizeOfAddrTable))
+        
+      } :+ ImageImportDescriptor(ImageThunkDataPointer(0), 0, 0, 0, ImageThunkDataPointer(0)) // terminating descriptor
 
+      def toAddressTable(extern: Extern) = extern.functionNames.map(getRVAAddress) :+ 0
       val importAddressTable = imports.flatMap(toAddressTable)
 
       return BoundImport(boundImports, importAddressTable)
@@ -106,7 +122,7 @@ case class Imports(val externs: Seq[Extern], val nonExterns: Seq[Extern], val of
 
     val getCompleteFunctionMap = {
       val flattenedFcns = imports.flatMap(imp => imp.functionNames ++ List(imp.dllName))
-      val start = offset + (imports.size + 1) * 20 + flattenedFcns.size * 4
+      val start = offset + numImportsPlusNull * ImageImportDescriptor.size + flattenedFcns.size * 4
       
       val mapEntries = new ListBuffer[(String, Int)]()
       var index = 0

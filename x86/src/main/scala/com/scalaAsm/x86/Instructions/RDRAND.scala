561cfeb0ee3ee67d3c74e03211bbcdd273f5c132
bdwashbu <bdwashbu@gmail.com>
2014-02-18 01:42:26 -0600

    minor tweaks to rdrand

--- x86/src/main/scala/com/scalaAsm/x86/Instructions/RDRAND.scala
+++ x86/src/main/scala/com/scalaAsm/x86/Instructions/RDRAND.scala
@@ -17,25 +17,20 @@ object RDRAND extends ModRM {
   abstract class M[X <: OperandSize](op1: ModRM.rm[X]) extends Instruction1[ModRM.rm[X]] {
      val opcodeExtension = Some(6.toByte)
      val operand1 = op1
+     val opcode = 0x0F.toByte
+     val opcode2 = Some(0xC7.toByte)
+     
   }
   
   implicit object rdrand1 extends RDRAND_1[rm32] {
     def get(x: rm32) = new M[DwordOperand](x) {
-      val opcode = 0x0F.toByte
-      val opcode2 = Some(0xC7.toByte)
       val modRM: Option[AddressingFormSpecifier] = Some(getAddressingFormExtended1(this))
-     }
+    }
   }
   
   implicit object rdrand2 extends RDRAND_1[rm16] {
     def get(x: rm16) = new M[WordOperand](x) {
-      val opcode = 0x0F.toByte
-      val opcode2 = Some(0xC7.toByte)
       val modRM: Option[AddressingFormSpecifier] = Some(getAddressingFormExtended1(this))
      }
   }
-  
-  //implicit object rdrand1 extends RDRAND_M[rm32] { def get(x: rm32) = Array[Byte](0x0F.toByte, 0xC7.toByte) ++ modRMExtended(x, extensionCode = 6.toByte).getBytes }
-  //implicit object rdrand2 extends RDRAND_M[rm16] { def get(x: rm16) = Array[Byte](0x0F.toByte, 0xC7.toByte) ++ modRMExtended(x, extensionCode = 6.toByte).getBytes }
-  //implicit object rdrand3 extends RDRAND_M[r64] { def get(x: r32) = Array[Byte](0x0F.toByte, 0xC7.toByte) ++ modRM(x, reg = 6.toByte) }
 }

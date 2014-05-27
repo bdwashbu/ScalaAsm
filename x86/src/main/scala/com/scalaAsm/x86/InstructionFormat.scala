67b5fcecc8159dcc5c7540e122bee555780cfe1a
bdwashbu <bdwashbu@gmail.com>
2014-05-26 22:15:18 -0500

    unnecessary code

--- x86/src/main/scala/com/scalaAsm/x86/InstructionFormat.scala
+++ x86/src/main/scala/com/scalaAsm/x86/InstructionFormat.scala
@@ -30,11 +30,11 @@ object OneOperandInstructionFormatter {
     (op1.base, op1.offset, op1.immediate) match {
       case (Some(base: Register64), _, _) =>
         WithSIB(ModRMOpcode(NoDisplacement, opcodeExtend.get, base), SIB(SIB.One, new ESP, base))
-      case (Some(base), offset @ Some(_: Displacement8), None) =>
+      case (Some(base), Some(_: Displacement8), None) =>
         NoSIB(ModRMOpcode(DisplacementByte, opcodeExtend.get, base))
       case (Some(base), None, None) =>
         NoSIB(ModRMOpcode(NoDisplacement, opcodeExtend.get, base))
-      case (None, None, imm @ Some(_: Immediate)) =>
+      case (None, None, Some(_: Immediate)) =>
         NoSIB(ModRMOpcode(NoDisplacement, opcodeExtend.get, new EBP))
       case _ => NoModRM()
     }
@@ -53,11 +53,11 @@ object TwoOperandInstructionFormatter {
 
   def encode(op1: GPR, op2: Memory, opcodeExtend: Option[Byte]): AddressingFormSpecifier = {
     (op2.base, op2.offset, op2.immediate) match {
-      case (Some(base), offset @ Some(off: Displacement8), None) if base.ID == 4 =>
+      case (Some(base), Some(off: Displacement8), None) if base.ID == 4 =>
         WithSIB(ModRMReg(DisplacementByte, op1, base), SIB(SIB.One, new ESP, base))
-      case (Some(base), offset @ Some(off: Displacement32), None) =>
+      case (Some(base), Some(off: Displacement32), None) =>
         NoSIB(ModRMReg(DisplacementDword, reg = op1, rm = base))
-      case (Some(base), offset @ Some(_: Displacement), None) =>
+      case (Some(base), Some(_: Displacement), None) =>
         NoSIB(ModRMReg(DisplacementByte, reg = op1, rm = base))
       case (Some(base), None, None) =>
         NoSIB(ModRMReg(NoDisplacement, reg = op1, rm = base))

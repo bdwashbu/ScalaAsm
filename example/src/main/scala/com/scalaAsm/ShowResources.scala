df1200e14166b5864d4c18b3182adb95ff3a4b6d
bdwashbu <bdwashbu@gmail.com>
2014-06-10 23:15:44 -0500

    Start working on outputting PE resources

---
+++ example/src/main/scala/com/scalaAsm/ShowResources.scala
@@ -0,0 +1,43 @@
+package com.scalaAsm
+
+import java.io._
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import com.scalaAsm.portableExe._
+import com.scalaAsm.portableExe.sections.Sections
+import com.scalaAsm.portableExe.sections.ImageResourceDirectory
+
+
+object ShowResources {
+
+  def main(args: Array[String]): Unit = {
+    try {
+
+      val file = new File("testResources.exe");
+	 
+	    val bFile: Array[Byte] = Array.fill(file.length().toInt)(0);
+	      
+	    //convert file into array of bytes
+	    val fileInputStream = new FileInputStream(file);
+	    fileInputStream.read(bFile);
+	    fileInputStream.close();
+	    
+	    val bbuf = ByteBuffer.wrap(bFile)
+	    bbuf.order(ByteOrder.LITTLE_ENDIAN)
+	    
+	    val dosHeader = DosHeader.getDosHeader(bbuf)
+	    val peHeader = PeHeader.getPeHeader(bbuf)
+	    val dirs = DataDirectories.getDirectories(bbuf)
+	    val sections = Sections.getSections(bbuf, peHeader.fileHeader.numberOfSections)
+      
+      val resourceRoot = ImageResourceDirectory.getResources(bbuf, sections, dirs.resource)
+
+      println("TOP LEVEL: ")
+      println(ImageResourceDirectory.getAllNamedEntries(resourceRoot).size)
+      ImageResourceDirectory.getAllNamedEntries(resourceRoot).foreach(x => println(x.name))
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+  }
+
+}

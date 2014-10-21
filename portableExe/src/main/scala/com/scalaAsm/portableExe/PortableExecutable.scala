d1d37f95d56f4264d3d9b32aaba55333bf4a3e8a
bdwashbu <bdwashbu@gmail.com>
2014-10-20 22:12:22 -0500

    Renamed "Assembled" to "Coff", "Linker64"->"Linker"

--- portableExe/src/main/scala/com/scalaAsm/portableExe/PortableExecutable.scala
+++ portableExe/src/main/scala/com/scalaAsm/portableExe/PortableExecutable.scala
@@ -1,45 +1,42 @@
 package com.scalaAsm.portableExe
 
-import com.scalaAsm.coff.SectionHeader
+import sections._
+import scala.collection.mutable.ArrayBuffer
+import com.scalaAsm.coff.Section
 
-trait PortableExecutable {
-  def get(): Array[Byte]
-}
+case class PortableExecutable(dosHeader: DosHeader,
+  peHeader: NtHeader,
+  directories: DataDirectories,
+  sections: Seq[Section]) {
 
-case class PortableExecutable32(dosHeader: DosHeader,
-						      peHeader: NtHeader,
-                 		      directories: DataDirectories,
-                 		      sections: Seq[SectionHeader],
-                 		      code: Array[Byte],
-                 		      rawData: Array[Byte],
-                 		      compiledImports: CompiledImports,
-                 		      resources: Array[Byte]) extends PortableExecutable
- {
-  
   def align(array: Array[Byte], to: Int, filler: Byte = 0xCC.toByte) = {
     val currentSize = array.size
     val numPadding = (to - (currentSize % to)) % to
     array ++: Array.fill(numPadding)(filler)
   }
-  
-  	def get(): Array[Byte] = {
-  	  val result = align(align(dosHeader(),16,0) ++:
-  	                                  peHeader() ++:
-  	                                  directories() ++:
-  	                                  sections.map(_.write).reduce(_ ++ _), 
-  	                                  peHeader.optionalHeader.sizeOfCode, 0x00) ++:
-  	               align(code, peHeader.optionalHeader.additionalFields.fileAlignment, 0x00) ++:
-  	               align(rawData ++ compiledImports.rawData, peHeader.optionalHeader.additionalFields.fileAlignment, 0x00) ++:
-  	               resources
-  	               
-  	  align(result, peHeader.optionalHeader.additionalFields.fileAlignment, 0x00)
-  	}
-  	
-  	override def toString = {
-  	  dosHeader.toString + "\n" +
-  	  peHeader.toString + "\n" +
-  	  directories.toString + "\n" +
-  	  sections.toString + "\n"
-  	}
-  	
- }
+
+  def get(): Array[Byte] = {
+    
+    val totalSize = sections.last.header.pointerToRawData + sections.last.header.sizeOfRawData
+    
+    var result = ArrayBuffer.fill(totalSize)(0.toByte)
+    
+    val headers = align(dosHeader(), 32, 0) ++:
+      peHeader() ++:
+      directories() ++:
+      sections.map(_.header.write).reduce(_ ++ _)
+    
+    result = result.patch(0, headers, headers.length)
+    sections.foreach{section => result = result.patch(section.header.pointerToRawData, section.contents, section.contents.length)}
+
+    result.toArray
+  }
+
+  override def toString = {
+    dosHeader.toString + "\n" +
+      peHeader.toString + "\n" +
+      directories.toString + "\n" +
+      sections.toString + "\n"
+  }
+
+}

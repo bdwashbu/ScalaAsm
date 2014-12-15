84641068a47082bdfef0f75c1a064f221dacd272
bdwashbu <bdwashbu@gmail.com>
2014-12-14 22:04:13 -0600

    more xml progress

--- example/src/main/scala/com/scalaAsm/ScalaBasic.scala
+++ example/src/main/scala/com/scalaAsm/ScalaBasic.scala
@@ -12,14 +12,18 @@ object ScalaBasic {
 
   case class x86InstructionDef(opcode: Int,
                                mnemonic: String,
-                               operand1: OperandDef,
-                               operand2: OperandDef,
+                               operands: Seq[OperandDef],
                                opcodeEx: Option[Int],
                                details: x86InstructionDetails) {
     override def toString = {
-      val first = "implicit object " + mnemonic + "_" + opcode + " extends " + mnemonic.toUpperCase() + "._2[" + operand1 + ", " + operand2 + "] {\n"
+      val first = if (operands.size == 2)
+        "implicit object " + mnemonic + "_" + opcode + " extends " + mnemonic.toUpperCase() + "._2[" + operands(0) + ", " + operands(1) + "] {\n"
+      else if (operands.size == 1)
+        "implicit object " + mnemonic + "_" + opcode + " extends " + mnemonic.toUpperCase() + "._1[" + operands(0) + "] {\n"
+      else
+        "implicit object " + mnemonic + "_" + opcode + " extends " + mnemonic.toUpperCase() + "._0 {\n"
       val second = first + "  def opcode = 0x" + opcode + "\n"
-      val third = if (operand1.operandType.map(_.promotedByRex).getOrElse(false) || operand2.operandType.map(_.promotedByRex).getOrElse(false)) {
+      val third = if (operands.map(_.operandType.map(_.promotedByRex).getOrElse(false)).contains(true)) {
         "  override def prefix = REX.W(true)\n"
       } else ""
       second + third + "}"
@@ -123,9 +127,18 @@ object ScalaBasic {
   object WordOrDoublewordExtendedToStack extends OperandType("16/32", true, false)
   object Word extends OperandType("16", false, false)
   object WordInteger extends OperandType("16int", false, false)
+  
+  object WordOrDoublewordBasedOnAddressSize extends OperandType("", false, false) // REP + LOOP
+  object DoublewordOrQuadwordBasedOnAddressSize extends OperandType("", false, false) // REP + LOOP
+  object WordBasedOnAddressSize extends OperandType("", false, false) // JCXZ
+  object WordBasedOnOperandSize extends OperandType("", false, false) // MOVSW
+  object WordBasedOnStackSize extends OperandType("", false, false) // PUSHF + POPF 64-bit
+  object DoublewordBasedOnAddressSize extends OperandType("", false, false) // JECXZ
+  object DoublewordBasedOnOperandSize extends OperandType("", false, false) // MOVSD
+  object QuadwordBasedOnAddressSize extends OperandType("", false, false) // JRCXZ
+  object QuadwordBasedOnOperandSize extends OperandType("", false, false) // PUSHFQ + POPFQ
 
   def decodeAddressingMethod(a: String): AddressingMethod = {
-    println(a)
     a match {
       case "A"   => DirectAddress
       case "BA"  => MemoryAddressedbyAX
@@ -160,7 +173,6 @@ object ScalaBasic {
   }
 
   def decodeOperandType(t: String): OperandType = {
-    println(t)
     t match {
       case "a"   => Two16or32ByteOperands
       case "b"   => ByteOperand
@@ -201,6 +213,15 @@ object ScalaBasic {
       case "vs"  => WordOrDoublewordExtendedToStack
       case "w"   => Word
       case "wi"  => WordInteger
+      case "va"  => WordOrDoublewordBasedOnAddressSize
+      case "dqa" => DoublewordOrQuadwordBasedOnAddressSize
+      case "wa" => WordBasedOnAddressSize
+      case "wo" => WordBasedOnOperandSize
+      case "ws" => WordBasedOnStackSize
+      case "da" => DoublewordBasedOnAddressSize
+      case "do" => DoublewordBasedOnOperandSize
+      case "qa" => QuadwordBasedOnAddressSize
+      case "qs" => QuadwordBasedOnOperandSize
     }
   }
 
@@ -219,56 +240,41 @@ object ScalaBasic {
   def loadXML() = {
 
     val xml = XML.loadFile("x86reference.xml")
-    val good = (xml \\ "pri_opcd")//.filter { x => (x \ "entry" \ "syntax" \ "mnem").text == "ADD"}
+    val pri_opcodes = (xml \\ "pri_opcd")
     
+    val defs = pri_opcodes.flatMap { pri_opcode =>
+      val opcode = Integer.parseInt(pri_opcode \@ "value", 16)
+
+      (pri_opcode \ "entry").filter{x => (x \ "syntax" \ "mnem").text == "ADD"}.map { entry =>
+        val mnemonic = (entry \ "syntax" \ "mnem").text
+        val opcodeEx = getOptionalInt(entry \ "opcd_ext")
+        val opSize = getOptionalBoolean(entry \ "@opsize")
+        val direction = getOptionalBoolean(entry \ "@direction")
+        val operands = (entry \ "syntax").flatMap{x => x \ "dst" ++ x \ "src"}
+        
+        val operandDefs = operands.map{ operand =>
+          val hasDetails = !(operand \ "a").isEmpty
+          val name = if (!hasDetails) Some(operand.text) else None
+          val opType =
+            if (!(operand \ "t").isEmpty)
+              Some(decodeOperandType((operand \ "t").text.trim))
+            else if (!(operand \ "@type").isEmpty)
+              Some(decodeOperandType((operand \ "@type").text.trim))
+            else
+              None
     
-    val defs = good.map { inst =>
-      val opcode = Integer.parseInt(inst \@ "value", 16)
-      println("WHAT " + opcode)
-      val mnemonic = (inst \ "entry" \ "syntax" \ "mnem").text
-      val opSize = getOptionalBoolean(inst \ "entry" \ "@opsize")
-      val direction = getOptionalBoolean(inst \ "entry" \ "@direction")
-      val dstOperand = inst \ "entry" \ "syntax" \ "dst"
-      val srcOperand = inst \ "entry" \ "syntax" \ "src"
-      val doesDstHaveDetails = !(dstOperand \ "a").isEmpty
-      val doesSrcHaveDetails = !(srcOperand \ "a").isEmpty
-      val dstName = if (!doesDstHaveDetails) Some(dstOperand.text) else None
-      val dstType =
-        if (!(dstOperand \ "t").isEmpty)
-          Some(decodeOperandType((dstOperand \ "t").text))
-        else if (!(dstOperand \ "@type").isEmpty)
-          Some(decodeOperandType((dstOperand \ "@type").text))
-        else
-          None
-
-      val dstAddressing =
-        if (!(dstOperand \ "a").isEmpty)
-          Some(decodeAddressingMethod((dstOperand \ "a").text))
-        else
-          None
-
-      val srcName = if (!doesSrcHaveDetails) Some(srcOperand.text) else None
-      val srcType =
-        if (!(srcOperand \ "t").isEmpty) {
-          println("really?: " + (srcOperand \ "t").text)
-          Some(decodeOperandType((srcOperand \ "t").text))
-        } else if (!(srcOperand \ "@type").isEmpty)
-          Some(decodeOperandType((srcOperand \ "@type").text))
-        else
-          None
-          
-      val srcAddressing =
-        if (!(srcOperand \ "a").isEmpty)
-          Some(decodeAddressingMethod((srcOperand \ "a").text))
-        else
-          None
-
-      val dst = OperandDef(dstName, dstType, dstAddressing)
-      val src = OperandDef(srcName, srcType, srcAddressing)
-      
-      val opcodeEx = getOptionalInt(inst \ "entry" \ "opcd_ext")
-      val details = x86InstructionDetails(opSize, direction)
-      x86InstructionDef(opcode, mnemonic, dst, src, opcodeEx, details)
+          val opAddressing =
+            if (!(operand \ "a").isEmpty)
+              Some(decodeAddressingMethod((operand \ "a").text.trim))
+            else
+              None
+              
+          OperandDef(name, opType, opAddressing)
+        }
+        
+        val details = x86InstructionDetails(opSize, direction)
+        x86InstructionDef(opcode, mnemonic, operandDefs, opcodeEx, details)
+      }
     }
     defs.foreach(println)
   }
